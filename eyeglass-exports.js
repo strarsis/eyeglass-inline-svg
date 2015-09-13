25f6b192d3a020c2658ea4eaa56ae2b85c360f61
strarsis <strarsis@gmail.com>
2015-09-13 19:39:45 +0200

    Move out functionality into separate eyeglass modules.

--- eyeglass-exports.js
+++ eyeglass-exports.js
@@ -1,22 +1,10 @@
 'use strict';
 
-var         path = require('path'),
-    inlineEscape = require('inline-escape'),
-              fs = require('fs');
+var path = require('path');
 
 module.exports = function(eyeglass, sass) {
   return {
     sassDir: path.join(__dirname, 'sass'),
-    // TODO: eyeglass assets?
-    functions: {
-      'inline-svg($path)': function(path, done) {
-        fs.readFile(path.getValue(), 'utf8', function (err,data) {
-        if (err) {
-          return console.log(err); // TODO: sass error object?
-        }
-        done(sass.types.String('url("data:image/svg+xml;charset=utf8,' + inlineEscape(data) + '")'));
-      });
-      }
-    }
+    functions: {}
   };
 };
